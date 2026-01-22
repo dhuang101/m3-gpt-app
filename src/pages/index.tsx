f4013880597796e533c9953b062d0e9dbd0ed754
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2026-01-22 15:54:49 +1100

    CSS adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 export default function Home() {
 	const [input, setInput] = useState("")
 	const [loading, setLoading] = useState(false)
-	const [prevText, setPrevText] = useState<string[]>(["input", "response"])
+	const [prevText, setPrevText] = useState<string[]>([])
 
 	function handleKeyDown(event: React.KeyboardEvent<HTMLTextAreaElement>) {
 		if (event.key === "Enter" && !event.shiftKey && input.trim() !== "") {
@@ -33,21 +33,24 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-screen bg-base-100">
 			{prevText.length > 0 ? (
-				<div className="flex flex-col items-center justify-center h-full w-1/2">
+				<div className="flex flex-col items-center h-full w-1/2">
 					<div className="flex flex-col h-4/5 w-full overflow-y-auto p-8">
 						{prevText.map((text, i) => {
 							if (i % 2 === 0) {
 								return (
 									<div
 										key={i}
-										className="bg-base-200 w-fit p-4 ml-auto rounded-b-xl rounded-tl-xl"
+										className="bg-base-300 w-fit max-w-2/3 p-4 ml-auto rounded-b-3xl rounded-tl-3xl"
 									>
 										{text}
 									</div>
 								)
 							} else if (i % 2 === 1) {
 								return (
-									<div key={i} className="my-4 mr-8">
+									<div
+										key={i}
+										className="bg-base-200 w-fit p-4 my-4 mr-8 rounded-b-3xl rounded-tr-3xl"
+									>
 										{text}
 									</div>
 								)
@@ -59,6 +62,9 @@ export default function Home() {
 						input={input}
 						setInput={setInput}
 					/>
+					<article className="mt-4 text-sm text-base-300">
+						disclaimer area
+					</article>
 				</div>
 			) : (
 				<div className="flex items-center justify-center w-1/2">
