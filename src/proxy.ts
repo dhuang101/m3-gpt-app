dc14d7163626af69f1ae6e19386b027da6c5f1b9
Derek Huang <derek.huang@monash.edu>
2026-01-28 17:33:04 +1100

    Added route protection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,23 @@
-import { NextRequest } from "next/server"
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-export async function proxy(req: NextRequest) {}
+export async function proxy(req: NextRequest) {
+	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+	const { pathname } = req.nextUrl
+
+	// Public paths
+	const publicPaths = ["/auth/signin"]
+	if (publicPaths.includes(pathname)) {
+		return NextResponse.next()
+	}
+
+	if (!token) {
+		return NextResponse.redirect(new URL("/auth/signin", req.url))
+	} else {
+		return NextResponse.next()
+	}
+}
 
 export const config = {
 	matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
