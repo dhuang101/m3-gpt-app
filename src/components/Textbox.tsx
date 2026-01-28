7d4e5e0e1d1f624aa47f8842942ee60191c16802
Derek Huang <derek.huang@monash.edu>
2026-01-29 00:43:46 +1100

    Moved alert to above the text area

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -54,7 +54,7 @@ function TextBox({
 			{showAlert && (
 				<div
 					role="alert"
-					className="alert alert-warning shadow-lg mb-4 absolute top-32 transition-all"
+					className="alert alert-warning shadow-lg mb-4 absolute -top-16 transition-all"
 				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -69,10 +69,7 @@ function TextBox({
 							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
 						/>
 					</svg>
-					<span>
-						BioMedGPT does not support images. Please switch to
-						MedGemma.
-					</span>
+					<span>BioMedGPT does not support images.</span>
 				</div>
 			)}
 
